6e0b6ec701cb32888a46a55bf1ee0726b301a28c
joaoat <stud.at@pm.me>
2026-07-22 15:03:06 +0100

    First commit: base springboot files generated by InteliJ IDEA

---
+++ src/main/kotlin/com/joaoat/library_backend_springboot_kt/LibraryBackendSpringbootKtApplication.kt
@@ -0,0 +1,11 @@
+package com.joaoat.library_backend_springboot_kt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LibraryBackendSpringbootKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<LibraryBackendSpringbootKtApplication>(*args)
+}
---
+++ src/test/kotlin/com/joaoat/library_backend_springboot_kt/LibraryBackendSpringbootKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.joaoat.library_backend_springboot_kt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibraryBackendSpringbootKtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
